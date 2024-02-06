0e569485300c23a580e05a3be624e1e1251733c8
Adrian Salceanu <essenciary@users.noreply.github.com>
2024-02-06 18:23:36 +0100

    Update main.jl

--- bin/main.jl
+++ bin/main.jl
@@ -1,6 +1,16 @@
-# This file is used for integration with JuliaHub
+# This script runs your Genie app as a JuliaHub Deployment
+
+# Start Genie from the app directory
+joinpath(@__DIR__, "..") |> cd
+
+# don't send command line arguments to Genie
+empty!(ARGS)
+
+# set up environment for production
+ENV["GENIE_ENV"] = "prod"
+ENV["GENIE_BANNER"] = false
+
+# load Genie app and start server
 using GenieFramework
 Genie.loadapp()
-include("app.jl")
-using .App
-Genie.up()
+up(; async=false, port=9999, host="0.0.0.0")
